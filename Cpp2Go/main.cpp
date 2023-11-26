8706dbb21cd178d482671a157049a83ae253f90e
Raziel_Gabay <razielg3306@gmail.com>
2023-11-27 00:46:05 +0200

    added basic getWord that right now support only SPACE as sign to end of the word

--- Cpp2Go/main.cpp
+++ Cpp2Go/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
 
+std::string getWord(std::string& code)
+{
+	std::string word;
+	for (auto it = code.begin(); it != code.end(); ++it)
+	{
+		word += *it;
+        if (*it == ' ')
+		{
+			code = std::string(it + 1, code.end());
+			return word;
+        }
+	}
+}
 
 int main()
 {
+	std::string code = "int main()\n{\n\tint a = 5;\n}";
 
 	return 0;
 }
