26be7bb6fb37325cb7d812cc6f11b9a0f2de1f92
shilo cohen <shilocohen02@gmail.com>
2023-11-27 17:35:04 +0200

    added functions - handleIdentifiers, deleteNewLine, deleteTab

--- Cpp2Go/main.cpp
+++ Cpp2Go/main.cpp
@@ -1,5 +1,51 @@
 #include <iostream>
+#include <vector>
 
+std::string deleteTab(std::string sourceCode)
+{
+	sourceCode.erase(std::remove(sourceCode.begin(), sourceCode.end(), '\t'), sourceCode.cend());
+	return sourceCode;
+}
+
+std::string deleteNewLine(std::string sourceCode)
+{
+	sourceCode.erase(std::remove(sourceCode.begin(), sourceCode.end(), '\n'), sourceCode.cend());
+	std::string code = deleteTab(sourceCode);
+	return code;
+}
+
+bool handleIdentifiers(std::string IdetifierToken)
+{
+	bool isValidIdentifier = true;
+	bool isALetter = isalpha(IdetifierToken[0]);
+	//make sure the first char is a letter or '_'
+	if ((IdetifierToken[0] != '_') && (isALetter == false))
+	{
+		isValidIdentifier = false;
+	}
+	//check if there are no special chars in the string
+	if (IdetifierToken[strspn(IdetifierToken.c_str(), "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_")] != 0)
+	{
+		isValidIdentifier = false;
+	}
+	//make sure there are no white spaces in the identifier string
+	for (int i = 0; i < IdetifierToken.size(); i++)
+	{
+		if (IdetifierToken[i] == ' ')
+			isValidIdentifier = false;
+	}
+	//make sure the identifier doesnt has a keyword name
+	char keyWordList[49][10] = { "asm", "auto", "break", "case", "catch", "char", "class", "const", "continue", "default", "delete", "do", "double", "else", "enum", "extern", "float", "for", "friend", "goto", "if", "inline", "int", "long", "new", "operator", "private", "protected", "public", "register", "return", "short", "signed", "sizeof", "static", "string", "struct", "switch", "template", "this", "throw", "try", "typedef", "union", "unsigned", "virtual", "void", "volatile", "while" };
+	for (int i = 0; i < 49; i++)
+	{
+		//comparing the name to all keywords in the list
+		if (strcmp(IdetifierToken.c_str(), keyWordList[i]) == 0)
+		{
+			isValidIdentifier = false;
+		}
+	}
+	return isValidIdentifier;
+}
 
 std::string getWord(std::string& code)
 {
