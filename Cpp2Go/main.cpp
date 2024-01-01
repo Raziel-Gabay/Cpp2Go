722101df438474291a1ee2ce76cfa5519137dd39
shilo cohen <shilocohen02@gmail.com>
2024-01-01 19:54:44 +0200

    finished function: readCodeFromFile, writeToDestFile

--- Cpp2Go/main.cpp
+++ Cpp2Go/main.cpp
@@ -2,6 +2,9 @@
 #include "parser.h"
 #include "AstTranslator.h"
 #include "codeGenerator.h"
+
+#include <fstream>
+
 void printAST(const ASTNode* node, int depth = 0) {
 	if (node) {
 		// Print current node
@@ -16,12 +19,37 @@ void printAST(const ASTNode* node, int depth = 0) {
 		}
 	}
 }
+std::string readCodeFromFile()
+{
+	std::string code = "";
+	std::fstream cppTextFile;
+	cppTextFile.open("cppCode.txt", std::ios::in); //open the file to read
+	if (cppTextFile.is_open())
+	{	//checking whether the file is open
+		std::string data;
+		while (getline(cppTextFile, data)) //read data from the text file
+		{
+			code += data + "\n";
+		}
+		cppTextFile.close(); //close the file object
+	}
+	return code;
+}
+
+void writeToDestFile(std::string goCode)
+{
+	std::cout << goCode;
+	std::ofstream goTextFile("goCode.txt");
+	if (goTextFile.is_open())
+	{
+		goTextFile << goCode;
+		goTextFile.close(); //close the file object
+	}
+}
 
 int main()
 {
-	//std::string code = "int a = 5;\n\tfloat b = 2.71;\n\tbool c = true;\n";
-	//std::string code = "int i = 10;\nstd::string str = \"hello\";\nfor(int i = 0; i < 10; i++)\n{\n\tif(i == 0)\n\t{\n\t\ti = 1;\n\t\ti = 2;\n\t}\n}";
-	std::string code = "int i = 0;\nif(i == 0)\n{\n\ti = 10;\n\ti = 20;\n}\nelse\n{\n\ti = 5;\n\ti = 8;\n}\ni =7;\n";
+	std::string code = readCodeFromFile();
 	std::cout << code << std::endl;
 	lexer::preprocessing(code);
 	std::cout << code << std::endl;
@@ -35,6 +63,7 @@ int main()
 		 printAST(translator.getAST());
 		 codeGenerator goCode = codeGenerator(translator.getAST());
 		 std::cout << goCode.getCode() << std::endl;
+		 writeToDestFile(goCode.getCode());
 	}
 	catch (const std::exception& e)
 	{
