bd888a16340228f34907551bd4b2ec2ec242fc70
Raziel_Gabay <razielg3306@gmail.com>
2024-01-23 00:34:21 +0200

    Code Generator Operator seperation

--- Cpp2Go/codeGenerator.cpp
+++ Cpp2Go/codeGenerator.cpp
@@ -168,31 +168,6 @@ void codeGenerator::generateBlock(ASTNode* node)
 		_countTab--;
 }
 
-void codeGenerator::generateExpression(ASTNode* node)
-{
-	_code += node->children.front()->value + " " +  node->value + " " + node->children.back()->value;
-	if (node->children.back()->name == ADDRESS_OF_OPERATOR)
-	{
-		if (node->children.back()->children.size() > 0)
-		{
-			_code += node->children.back()->children.front()->value;
-		}
-	}
-}
-
-void codeGenerator::generateExpression(std::vector<ASTNode*> nodes)
-{
-	std::string last_value = nodes.front()->children.back()->value;
-	for (ASTNode* node : nodes)
-	{
-		if (node->children.front()->value == last_value)
-			_code += " " + node->value + " " + node->children.back()->value;
-		else
-			generateExpression(node);
-		last_value = node->children.back()->value;
-	}
-}
-
 void codeGenerator::generateIncludeDirective(ASTNode* node)
 {
 	for (ASTNode* child : node->children)
