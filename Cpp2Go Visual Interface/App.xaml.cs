7c6fad33448492b0a5459e4daca8e9b23693456e
Raziel_Gabay <razielg3306@gmail.com>
2024-01-31 19:45:48 +0200

    half finished - start screen

---
+++ Cpp2Go Visual Interface/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Cpp2Go_Visual_Interface
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
