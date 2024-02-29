7208328b71fc7332615b29679df551557c1c0634
shilo cohen <shilocohen02@gmail.com>
2024-03-01 01:02:45 +0200

    started the opening screen

---
+++ Cpp2Go GUI/project/src/components/codeArea.tsx
@@ -0,0 +1,22 @@
+function CodeArea() {
+  return (
+    <div>
+        <br></br>
+        <br></br>
+        <h1><i><big>Welcome to Cpp2Go transpiler!</big></i></h1>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <a href="https://www.google.com"
+        target="_blank">
+            <button title= "open the website" style={{fontSize: 30, backgroundColor: "black", color: "white", borderRadius: 15}}> click here to start</button>
+        </a>
+        
+    </div>
+    
+  )
+}
+
+export default CodeArea
